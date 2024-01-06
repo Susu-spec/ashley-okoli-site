b865f1e38cb49d26d01578dcb16b6427d802fc08
Susu-spec <suleimansuwaibat@gmail.com>
2024-01-06 17:32:55 +0100

    Update: Clearer content structure

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-/**Add menu item dash
+/**
+ * Add menu item dash
  * Translate along the x axis and as content loads,
  * scale along x-axis and scale shortly on y-axis
  */
@@ -6,3 +7,12 @@
 /**
  * Set right cell to have background and image to fade out
  */
+
+/**
+ * Tap on button to remove div and add the button 
+ * element's div name
+ */
+const buttons = document.getElementsByTagName("button");
+      pages = document.querySelector(".page");
+      active = document.querySelector(".active");
+      inactive = document.querySelector(".inactive");
